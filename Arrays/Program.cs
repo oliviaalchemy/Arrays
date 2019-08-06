2f9aec91a5da07b4855b47d871e9411ca1d2af13
oliviaalchemy <olivia@alchemytechs.com>
2019-08-06 14:53:58 +0100

    Add project files.

---
+++ Arrays/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] luckyNumbers = { 4, 8, 15, 16, 23, 42 };
+            string[] friends = new string[5];
+            friends[0] = "Jim";
+            friends[1] = "Kelly";
+
+            Console.WriteLine(luckyNumbers[1]); 
+
+            Console.ReadLine();
+        }
+    }
+}
